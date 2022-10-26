64f18d67159a8f5bec06e6336634bcf25ce70e23
AProzorov <aprozorov@inno.tech>
2022-10-26 12:45:13 +0300

    Added sub tests for tcp checker

--- checker/checker.go
+++ checker/checker.go
@@ -1,5 +1,88 @@
-package main
+package checker
 
-func main() {
+import (
+	"fmt"
+	"net"
+	"sort"
+	"strconv"
+	"strings"
+)
 
+func ConcurrentScanning(address string, networkType string) {
+	ports := make(chan int, 10)
+	results := make(chan int)
+	var openports []int
+	for i := 0; i < cap(ports); i++ {
+		go dialSync(ports, results, address, networkType)
+	}
+
+	go func() {
+		for i := 0; i < 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, openport := range openports {
+		fmt.Println("Port ", openport, " is open")
+	}
+}
+
+func dialSync(ports, result chan int, address, networkType string) {
+	for port := range ports {
+		builder := strings.Builder{}
+		builder.WriteString(address)
+		builder.WriteString(":")
+		builder.WriteString(strconv.Itoa(port))
+		addressWithPort := builder.String()
+		conn, err := net.Dial(networkType, addressWithPort)
+		if err != nil {
+			//fmt.Println("Error dial to: ", addressWithPort)
+			result <- 0
+			continue
+		} else {
+			//fmt.Println("Success dial to: ", addressWithPort)
+			result <- port
+		}
+		err = conn.Close()
+		if err != nil {
+			// Skip error
+			continue
+		}
+	}
+}
+
+func NonConcurrentScanning(address string, networkType string) {
+	for i := 0; i < 1024; i++ {
+		builder := strings.Builder{}
+		builder.WriteString(address)
+		builder.WriteString(":")
+		builder.WriteString(strconv.Itoa(i))
+		addressWithPort := builder.String()
+		dial(addressWithPort, networkType)
+	}
+}
+
+func dial(address string, networkType string) {
+	conn, err := net.Dial(networkType, address)
+	if err != nil {
+		//fmt.Println("Error dial to: ", address)
+		return
+	} else {
+		fmt.Println("Success dial to: ", address)
+	}
+	err = conn.Close()
+	if err != nil {
+		// Skip error
+		return
+	}
 }
--- checker/checker_test.go
+++ checker/checker_test.go
@@ -1,17 +1,35 @@
-package main
+package checker
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestConcurrentScanning(t *testing.T) {
+func BenchmarkConcurrentScanning(b *testing.B) {
 	address := "scanme.nmap.org"
 	network := "tcp"
 
 	ConcurrentScanning(address, network)
 }
 
-func TestNonConcurrentScanning(t *testing.T) {
+func BenchmarkNonConcurrentScanning(b *testing.B) {
 	address := "scanme.nmap.org"
 	network := "tcp"
 
 	NonConcurrentScanning(address, network)
+}
+
+func TestNonConcurrentScanning(t *testing.T) {
+	address := "scanme.nmap.org"
+	network := "tcp"
+
+	t.Run("A1", func(t *testing.T) {
+		address = "scanme.nmap.org"
+		network = "tcp"
+		ConcurrentScanning(address, network)
+	})
+	t.Run("A2", func(t *testing.T) {
+		address = "ya.ru"
+		network = "tcp"
+		ConcurrentScanning(address, network)
+	})
 }
